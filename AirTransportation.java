77b09f01be200e1a36313a958d834f580eb641d5
Ian Resende <202100243@estudantes.ips.pt>
2024-04-03 11:46:25 +0100

    level 3 (completo)

--- AirTransportation.java
+++ AirTransportation.java
@@ -7,4 +7,24 @@ public class AirTransportation extends Transport{
         int numberOfContainers;
         super.setFees(airfees);
     }
+
+    @Override
+    public String getTransportType(){
+        return "Air transport";
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("\n%15s: %s\n", "Tipo Transporte", getTransportType()));
+        sb.append(String.format("%15s: %s\n", "ID", getId()));
+        sb.append(String.format("%15s: %s\n", "Origem", getOrigin()));
+        sb.append(String.format("%15s: %s\n", "Destino", getDestination()));
+        sb.append(String.format("%15s: %5.2f€\n", "Preço", getPrice()));
+        sb.append(String.format("%15s: %4.2f%%\n", "Honorarios", getFees()));
+        sb.append(String.format("%15s: %4.2f€\n", "Preço Final", getPriceWithFees()));
+
+        return sb.toString();
+    }
 }
--- GroundTransportation.java
+++ GroundTransportation.java
@@ -6,4 +6,24 @@ public class GroundTransportation extends Transport{
         super.setFees(groundFees);
         String licensePlate;
     }
+
+    @Override
+    public String getTransportType(){
+        return "ground transport";
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("\n%15s: %s\n", "Tipo Transporte", getTransportType()));
+        sb.append(String.format("%15s: %s\n", "ID", getId()));
+        sb.append(String.format("%15s: %s\n", "Origem", getOrigin()));
+        sb.append(String.format("%15s: %s\n", "Destino", getDestination()));
+        sb.append(String.format("%15s: %5.2f€\n", "Preço", getPrice()));
+        sb.append(String.format("%15s: %4.2f%%\n", "Honorarios", getFees()));
+        sb.append(String.format("%15s: %4.2f€\n", "Preço Final", getPriceWithFees()));
+
+        return sb.toString();
+    }
 }
