1473b928b5fa41860a15fd010297a64f7c3eda89
Ian Resende <202100243@estudantes.ips.pt>
2024-04-03 11:35:37 +0100

    level 1,2,3

---
+++ AirTransportation.java
@@ -0,0 +1,10 @@
+public class AirTransportation extends Transport{
+
+    private static final double airfees= 0.04;
+    public AirTransportation(){
+        super();
+        String name;
+        int numberOfContainers;
+        super.setFees(airfees);
+    }
+}
---
+++ GroundTransportation.java
@@ -0,0 +1,9 @@
+public class GroundTransportation extends Transport{
+
+    private static final double groundFees= 0.03;
+    public GroundTransportation(){
+        super();
+        super.setFees(groundFees);
+        String licensePlate;
+    }
+}
